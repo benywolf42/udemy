f1177331e8205c6085471c97740da649c88d52b0
DESKTOP-N9TGRGQ\Gustavo <gustavodelgado@pm.me>
2019-03-18 21:24:37 -0300

    cs: exercicio IF 18-03

---
+++ Exercicios 18-03/Exercicios 18-03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios_18_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public double calculaNotas(double valor)
+        {
+
+        }
+
+
+    }
+}
