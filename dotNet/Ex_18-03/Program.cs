14bc2ddb29f34f4c7231c4439bb9d947fb2dd7f7
Gustavo Delgado <gustavo.delgado@voxage.com.br>
2019-03-22 17:26:50 -0300

    Questões 7 à 9

--- dotNet/Ex_18-03/Program.cs
+++ dotNet/Ex_18-03/Program.cs
@@ -14,7 +14,9 @@ namespace dotNet
             // calculoEntreDatas("29/02/2016");
             // maiorNumero(1,45);
             // isAnoBissexto();
-
+            // calculoIMC(86, 1.75);
+            // listagemDDDs();
+            calculadora();
         }
 
         // QUESTAO 1
@@ -41,7 +43,6 @@ namespace dotNet
             
             
         }
-
         // QUESTAO 2
         public static void calculoEntreDatas(string dataInicial)
         {
@@ -80,13 +81,12 @@ namespace dotNet
                 Console.WriteLine("Data Inválida!");
             }
         }
-
         // QUESTAO 3
         public static void maiorNumero(double n1, double n2) {
             double maiorNum = n2 > n1 ? n2 : n1;
             Console.WriteLine(maiorNum);
         }
-
+        // QUESTAO 6
         public static void isAnoBissexto() {
             Console.WriteLine("Digite um ano a ser verificado: ");
             int ano = Convert.ToInt32(Console.ReadLine());
@@ -98,6 +98,126 @@ namespace dotNet
                 Console.WriteLine("O ano " + ano + " é bissexto.");
             } else {
                 Console.WriteLine("O ano " + ano + " não é bissexto.");
+            }
+        }
+        // QUESTAO 7
+        public static void calculoIMC(double peso, double altura) {
+            double IMC = peso/(altura*altura);
+            if (IMC < 18.5) {
+                Console.WriteLine("Você está abaixo do peso.");
+            } else if (IMC >= 18.5 & IMC < 25) {
+                Console.WriteLine("Você está no seu peso normal.");
+            } else if (IMC >= 25 & IMC < 30) {
+                Console.WriteLine("Você está com sobrepeso.");
+            } else if (IMC >= 30 & IMC < 35) {
+                Console.WriteLine("Você está com obesidade grau I.");
+            } else if (IMC >= 35 & IMC < 40) {
+                Console.WriteLine("Você está com obesidade grau II.");
+            } else if (IMC > 40) {
+                Console.WriteLine("Você está com obesidade grau III.");
+            } else 
+                Console.WriteLine("Erro.");
+        }
+        // QUESTAO 8
+        public static void listagemDDDs() {
+            Console.WriteLine("Digite o seu DDD: ");
+            int input_ddd = Convert.ToInt32(Console.ReadLine());
+
+            switch (input_ddd)
+            {
+                case 11: {
+                    Console.WriteLine("São Paulo - SP");
+                    break;
+                }
+                case 21: {
+                    Console.WriteLine("Rio de Janeiro - RJ");
+                    break;
+                }
+                case 27: {
+                    Console.WriteLine("Vitória - ES");
+                    break;
+                }
+                case 31: {
+                    Console.WriteLine("Belo Horizonte - MG");
+                    break;
+                }
+                case 41: {
+                    Console.WriteLine("Curitiba - PR");
+                    break;
+                }
+                case 48: {
+                    Console.WriteLine("Florianópolis - SC");
+                    break;
+                }
+                case 51: {
+                    Console.WriteLine("Porto Alegre - RS");
+                    break;
+                }
+                case 61: {
+                    Console.WriteLine("Brasília - DF");
+                    break;
+                }
+                default: {
+                    Console.WriteLine("Parâmetro inválido");
+                    break;
+                }
+            }
+        }
+        // QUESTAO 9
+        public static void calculadora() {
+            Console.WriteLine("=================");
+            Console.WriteLine("1 - soma    ");
+            Console.WriteLine("2 - subtração");
+            Console.WriteLine("3 - multiplicação");
+            Console.WriteLine("4 - divisão");
+            Console.WriteLine("=================");
+            Console.WriteLine("Digite a opção desejada: ");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            double n1, n2;
+
+            switch (opcao)
+            {
+                case 1: {
+                    Console.WriteLine("Digite o primeiro número a ser somado: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número a ser somado: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(n1 + n2);
+                    break;
+                }
+                case 2: {
+                    Console.WriteLine("Digite o primeiro número a ser subtraído: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número a ser subtraído: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(n1 - n2);
+                    break;
+                }
+                case 3: {
+                    Console.WriteLine("Digite o primeiro número a ser multiplicado: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número a ser multiplicado: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(n1 * n2);
+                    break;
+                }
+                case 4: {
+                    Console.WriteLine("Digite o primeiro número a ser dividido: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número a ser dividido: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    if (n2 == 0){
+                        Console.WriteLine("Não existe divisão por zero");
+                        break;
+                    }
+                    Console.WriteLine(n1 / n2);
+                    break;
+                }
+                default: {
+                    Console.WriteLine("Sem tempo irmão.");
+                    break;
+                }
             }
         }
     }
